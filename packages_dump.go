4f4b06875a51df5febc16e2c82e60eac4bc31502
Tobias Hintze <thintze+git@tpip.net>
2016-02-12 09:37:30 +0100

    make -dump mode recursive too

--- packages_dump.go
+++ packages_dump.go
@@ -12,31 +12,42 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"time"
 )
 
-func dumpPackages(folder string, nworkers int, doMD5, doSHA1 bool) {
+func dumpPackages(root string, nworkers int, doMD5, doSHA1 bool) {
 
-	var (
-		scanner = packageScanner{
-			root:   folder,
-			doMD5:  doMD5,
-			doSHA1: doSHA1,
+	filepath.Walk(root, func(path string, fi os.FileInfo, walkerr error) error {
+		if fi == nil {
+			log.Printf("no such file or directory: %s\n", path)
+			return nil
 		}
-		now = time.Now()
-		err error
-	)
-	log.Println("start building index from", folder)
+		if !fi.IsDir() {
+			return nil
+		}
 
-	if err = scanner.scan(nworkers); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(2)
-	}
+		var (
+			scanner = packageScanner{
+				root:   path,
+				doMD5:  doMD5,
+				doSHA1: doSHA1,
+			}
+			now = time.Now()
+			err error
+		)
+		log.Println("start building index from", path)
 
+		if err = scanner.scan(nworkers); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(2)
+		}
+		log.Printf("time to parse %d packages: %s\n",
+			scanner.packages.Len(), time.Since(now))
+		scanner.packages.StringTo(os.Stdout)
+		return nil
+	})
 	log.Println("done building index")
-	log.Printf("time to parse %d packages: %s\n",
-		scanner.packages.Len(), time.Since(now))
 
-	scanner.packages.StringTo(os.Stdout)
 	return
 }
